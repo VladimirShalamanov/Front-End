50d391ca3475071b31b99c958d812efb0d7e87d0
VladimirShalamanov <102071427+VladimirShalamanov@users.noreply.github.com>
2024-02-10 20:36:53 +0200

    Created src components

---
+++ ReactJS/04.Components - workshop-user-list/client/src/components/UserListTable.jsx
@@ -0,0 +1,179 @@
+import { useState, useEffect } from "react";
+
+import * as userService from "../services/userService";
+import CreateUserModal from "./CreateUserModal";
+import UserDeleteModal from "./UserDeleteModal";
+import UserInfoModal from "./UserInfoModal";
+import UserListItem from "./UserListItem";
+import Spinner from "./Spinner";
+import NoUsers from "./NoUsers";
+
+const UserListTable = () => {
+    const [users, setUsers] = useState([]);
+
+    const [selectedUser, setSelectedUser] = useState(null);
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [showCreate, setShowCreate] = useState(false);
+    const [showInfo, setShowInfo] = useState(false);
+    const [showDelete, setShowDelete] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+
+        userService.getAll()
+            .then(result => setUsers(result))
+            .catch(error => console.log(error))
+            .finally(() => setIsLoading(false));
+    }, []);
+
+    const createUserClickHandler = () => {
+        setShowCreate(true);
+    };
+
+    const hideCreateUserModal = () => {
+        setShowCreate(false);
+    };
+
+    const userCreateHandler = async (e) => {
+        e.preventDefault();
+
+        const data = Object
+            .fromEntries(new FormData(e.currentTarget));
+
+        const newUser = await userService.create(data);
+        setUsers(state => [...state, newUser]);
+
+        setShowCreate(false);
+    };
+
+    const userInfoClickHandler = async (userId) => {
+        setSelectedUser(userId);
+        setShowInfo(true);
+    };
+
+    const deleteUserClickHandler = (userId) => {
+        setSelectedUser(userId);
+        setShowDelete(true);
+    };
+
+    const deleteUserHandler = async () => {
+        await userService.remove(selectedUser);
+
+        setUsers(state => state.filter(u => u._id !== selectedUser))
+
+        setShowDelete(false);
+    };
+
+    return (
+        <div className="table-wrapper">
+            {showCreate &&
+                <CreateUserModal
+                    onClose={hideCreateUserModal}
+                    onCreate={userCreateHandler}
+                />
+            }
+
+            {showInfo && (
+                <UserInfoModal
+                    onClose={() => setShowInfo(false)}
+                    userId={selectedUser}
+                />
+            )}
+
+            {showDelete && (
+                <UserDeleteModal
+                    onClose={() => setShowDelete(false)}
+                    onDelete={deleteUserHandler}
+                />
+            )}
+
+            {isLoading && <Spinner />}
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>
+                            Image
+                        </th>
+                        <th>
+                            First name<svg aria-hidden="true" focusable="false" data-prefix="fas"
+                                data-icon="arrow-down" className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn"
+                                role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                                <path fill="currentColor"
+                                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                                </path>
+                            </svg>
+                        </th>
+                        <th>
+                            Last name<svg aria-hidden="true" focusable="false" data-prefix="fas"
+                                data-icon="arrow-down" className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn"
+                                role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                                <path fill="currentColor"
+                                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                                </path>
+                            </svg>
+                        </th>
+                        <th>
+                            Email<svg aria-hidden="true" focusable="false" data-prefix="fas"
+                                data-icon="arrow-down" className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn"
+                                role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                                <path fill="currentColor"
+                                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                                </path>
+                            </svg>
+                        </th>
+                        <th>
+                            Phone<svg aria-hidden="true" focusable="false" data-prefix="fas"
+                                data-icon="arrow-down" className="icon svg-inline--fa fa-arrow-down Table_icon__+HHgn"
+                                role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                                <path fill="currentColor"
+                                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                                </path>
+                            </svg>
+                        </th>
+                        <th>
+                            Created
+                            <svg aria-hidden="true" focusable="false" data-prefix="fas"
+                                data-icon="arrow-down" className="icon active-icon svg-inline--fa fa-arrow-down Table_icon__+HHgn"
+                                role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                                <path fill="currentColor"
+                                    d="M374.6 310.6l-160 160C208.4 476.9 200.2 480 192 480s-16.38-3.125-22.62-9.375l-160-160c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 370.8V64c0-17.69 14.33-31.1 31.1-31.1S224 46.31 224 64v306.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0S387.1 298.1 374.6 310.6z">
+                                </path>
+                            </svg>
+                        </th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                    {users.length ?
+                        users.map(u => (
+                            <UserListItem
+                                key={u._id}
+                                userId={u._id}
+                                createdAt={u.createdAt}
+                                email={u.email}
+                                firstName={u.firstName}
+                                imageUrl={u.imageUrl}
+                                lastName={u.lastName}
+                                phoneNumber={u.phoneNumber}
+                                onInfoClick={userInfoClickHandler}
+                                onDeleteClick={deleteUserClickHandler}
+                            />
+                        ))
+                        :
+                        <NoUsers />
+                    }
+
+
+                </tbody>
+            </table>
+
+            <button className="btn-add btn"
+                onClick={createUserClickHandler}>Add new user</button>
+        </div>
+    );
+}
+
+export default UserListTable;
